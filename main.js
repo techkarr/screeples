c73728c7c1af899a52bbddb678296c0398661d6c
techkarr <88687157+techkarr@users.noreply.github.com>
2023-04-20 09:56:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,28 +18,6 @@
 	
 	*/
 	
-	
-
-for (const roomName in Game.rooms) {
-	const room = Game.rooms[roomName];
-	const spawns = room.find(FIND_MY_SPAWNS);
-	const flags = room.find(FIND_FLAGS);
-	const sources = room.find(FIND_SOURCES);
-	const screeps = room.find(FIND_MY_CREEPS);
-	
-		for (const spawn of spawns) {
-			
-		}
-};
-
-
-
-
-
-
-
-
-
 
 
 
